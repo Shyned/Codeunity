4376d96932efaa74d34657104787bae59fa51462
Shyned <eyedolarts@gmail.com>
2022-10-30 18:04:42 -0500

    initailized react native project and created pages for login,signup, and chatroom

---
+++ Codeunity_frontend/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Button } from "react-native-paper";
+
+export default function Navbar() {
+  // logic
+  return (
+    <View>
+      <Text>nav</Text>
+    </View>
+  );
+}
+
+// const styling
