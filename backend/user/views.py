43aa3b4eba3f4ca817224c7c11a62404a4a2f7af
Shyned <eyedolarts@gmail.com>
2022-10-30 20:17:19 -0500

    created views to get user

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+# 
+from user.serializer import UserSerializer
+# 
+from .models import UserModel
+#
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 # Create your views here.
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_user(request):
+    user = UserModel.objects.filter(user_firebase_id = request.fire_base_id)
+    serializer = UserSerializer(user,many= False)
+    return Response(serializer.data)
