546ad65c0a86d682074cd3d78aa3704091a66572
Shyned <eyedolarts@gmail.com>
2022-11-01 22:59:04 -0500

    installed supabase

--- backend/user/serializer.py
+++ backend/user/serializer.py
@@ -5,5 +5,5 @@ from .models import UserModel
 class UserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = UserModel
-        fields = ['user_firebase_id', 'user_name', 'first_name', 'last_name','date_of_birth']
+        fields = ['id','user_firebase_id', 'user_name', 'first_name', 'last_name','date_of_birth']
         depth = 1
