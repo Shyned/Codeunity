05f69c6464f673329c308071b772a93168922446
Shyned <eyedolarts@gmail.com>
2022-11-22 23:01:13 -0600

    added urls to apps and serializer to User

--- codeunity_backend/user/models.py
+++ codeunity_backend/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserModel(models.Model):
+    user_name = models.CharField(max_length = 20, unique = True )
+    first_name = models.CharField(max_length = 20, unique = True )
+    last_name = models.CharField(max_length = 20 )
+    date_of_birth = models.DateField()
+    
