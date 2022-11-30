72cd53f2f32e351ccce7a853564de49786bd0023
Shyned <eyedolarts@gmail.com>
2022-11-29 23:39:38 -0600

    created react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default App;
