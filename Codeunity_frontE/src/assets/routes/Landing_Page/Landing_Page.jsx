e5f9c6aec212d44fd1dbe1bdf8a42884f649e9b8
Shyned <eyedolarts@gmail.com>
2022-12-04 16:19:39 -0600

    styling and adding functionality to the landing page

--- Codeunity_frontE/src/assets/routes/Landing_Page/Landing_Page.jsx
+++ Codeunity_frontE/src/assets/routes/Landing_Page/Landing_Page.jsx
@@ -1,9 +1,19 @@
 // import CSS
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
+import { Link, Stack } from "@mui/material";
 
 export default function Landing_Page() {
   return (
     <div>
-      <h1>Landing Page</h1>
+      <Stack spacing={1} alignItems="flex-end">
+        <Link>Sign In</Link>
+        <Link>Register</Link>
+      </Stack>
+
+      <h1>Codeunity</h1>
+      <h2>Work Play Communicate</h2>
     </div>
   );
 }
