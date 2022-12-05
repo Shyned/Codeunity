6ba60baf62f835df984feaf99dcc23dd38943c3e
Shyned <eyedolarts@gmail.com>
2022-12-05 09:44:30 -0600

    created nav links for signing in and logging in

--- Codeunity_frontE/src/assets/routes/Landing_Page/Landing_Page.jsx
+++ Codeunity_frontE/src/assets/routes/Landing_Page/Landing_Page.jsx
@@ -2,14 +2,16 @@
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Box from "@mui/material/Box";
-import { Link, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Landing_Page() {
   return (
     <div>
-      <Link to="login/">Sign In</Link>
-      <Link to="signup/">Register</Link>
-
+      <Stack spacing={2} alignItems="flex-end">
+        <Link to="login/">Sign In</Link>
+        <Link to="signup/">Register</Link>
+      </Stack>
       <h1>Codeunity</h1>
       <h2>Work Play Communicate</h2>
     </div>
