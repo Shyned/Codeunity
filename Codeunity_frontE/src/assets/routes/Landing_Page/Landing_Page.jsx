607a3e0a8d78faba8d80bc736f9de64862db9a26
Shyned <eyedolarts@gmail.com>
2022-12-15 18:12:52 -0600

    made dark theme and added comment, navbar, and open chatrooms components

--- Codeunity_frontE/src/assets/routes/Landing_Page/Landing_Page.jsx
+++ Codeunity_frontE/src/assets/routes/Landing_Page/Landing_Page.jsx
@@ -4,10 +4,28 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import Box from "@mui/material/Box";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function Landing_Page() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  const LightTheme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
+
   return (
     <div>
+      <ThemeProvider theme={LightTheme}>
+        <CssBaseline />
+        <main>This app is using the dark mode</main>
+      </ThemeProvider>
       <Stack spacing={2} alignItems="flex-end">
         <Link to="login/">Sign In</Link>
         <Link to="signup/">Register</Link>
